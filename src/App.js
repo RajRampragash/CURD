a22604a16215cdbf697741791a2a43c19133fb99
Mathew <119226957+RajRampragash@users.noreply.github.com>
2023-05-05 17:05:12 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+// import { useState } from 'react';
+import './App.css';
+// import Emx from './Components/Emx/exm';
+import Curd from './Components/Curd/curd';
+
+
+
+
+
+function App() {
+  // const [name, setName] = useState("kalai")
+  // const [list, setList] = useState(["raj", "vicky", "varsha"])
+  return (
+    <div className="App">
+      <h3><b>basice function of CURD </b></h3>
+      <Curd />
+
+    </div>
+  );
+}
+
+export default App;
